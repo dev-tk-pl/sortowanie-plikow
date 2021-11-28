18770ca6c4ebdabad90ebfc47f8be7aa8461430f
dev_tk <dev_tk@int.pl>
2021-11-28 21:31:01 +0100

    fix-add dirPath

--- node-file-sort.js
+++ node-file-sort.js
@@ -1,34 +1,5 @@
 const fs = require('fs');
 
-// const getSortedFiles = async (dir) => {
-//   const files = await fs.promises.readdir(dir);
-//   var myPath = './tmp';
-
-//   console.log({files});
-
-// files.forEach(f => {
-//   const { birthtime } = fs.statSync(f)
-//   console.log(birthtime);
-// })
-
-// if (!fs.existsSync(myPath)){
-//     fs.mkdirSync(myPath);
-// }
-
-// return files
-//   .map(fileName => ({
-//     name: fileName,
-//     time: fs.statSync(`${dir}/${fileName}`).mtime.getTime(),
-//   }))
-//   .sort((a, b) => a.time - b.time)
-//   .map(file => file.name);
-// };
-
-// Promise.resolve()
-//   .then(() => getSortedFiles(myDir))
-//   .then(console.log)
-//   .catch(console.error);
-
 const selectFolder = require('win-select-folder');
 const root = 'myComputer'; // rootfolder - default desktop
 const description = 'Wybierz folder do posortowania'; // default Select Folder
@@ -51,13 +22,12 @@ const selectFolder2sort = async (dir) => {
     fs.stat(`${dir}/${fileName}`, (err, stats) => {
       if (err) { console.log(err) }
       else {
-        console.log(fileName);
+        // console.log(fileName);
         let fileDate = stats.birthtime.toISOString().slice(0, 10);
         let fileTime = stats.birthtime.toISOString().slice(11, 19);
-        console.log('data : ', fileDate)
-        console.log('godz. : ', fileTime)
-        console.log(parseInt(fileTime.replace(/:/g, "")))
-
+        // console.log('data : ', fileDate)
+        // console.log('godz. : ', fileTime)
+        // console.log(parseInt(fileTime.replace(/:/g, "")))
         sortFileByDate(fileDate, fileTime, fileName, dir)
       }
     })
@@ -74,13 +44,12 @@ function sortFileByDate(dateStart, time, fileName, dirPath) {
   let check_val = Date.parse(`${dateStart}T${time.slice(0, 5)}`);
 
   if (check_val > open_time && check_val < close_time) {
-    console.log('one day');
-    let dir = `./test/${dateStart}`;
+    let dir = `${dirPath}/${dateStart}`;
     if (!fs.existsSync(dir)) {
       fs.mkdirSync(dir);
     }
-    let oldPath = `./test/${fileName}`
-    let newPath = `./test/${dateStart}/${fileName}`
+    let oldPath = `${dirPath}/${fileName}`
+    let newPath = `${dirPath}/${dateStart}/${fileName}`
 
     fs.rename(oldPath, newPath, function (err) {
       if (err) throw err
